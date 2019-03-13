6c6786667c149fc521311a477dee3c160b0c79f5
Williams Argenal <williamsargenal@gmail.com>
2019-03-13 13:45:28 -0700

    adding app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 console.log('logged in randomintinclusive');
 var firstAndPike = {
