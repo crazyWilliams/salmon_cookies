ff15791e7da509f5199a6d7aac200cc78db5ff20
Williams Argenal <williamsargenal@gmail.com>
2019-03-12 17:09:51 -0700

    fixed the times in my cookies

--- app.js
+++ app.js
@@ -32,20 +32,19 @@ var firstAndPike = {
   };
 firstAndPike.calculateSalesPerHour();
 
-
-
 var firstAndPike_ul = document.getElementById('firstAndPike');
 var title_li = document.createElement('li');
 
 title_li.textContent = '1st and Pike';
 firstAndPike_ul.appendChild(title_li);
 console.log('testing');
-
-
+var times = [6 + ':00am ',7 + ':00am ',8 + ':00am ',9 + ':00am ',10 + ':00am ',11 + ':00am ',12 + ':00pm ',1 + ':00pm ',2 + ':00pm ',3 + ':00pm ',4 + ':00pm ',5 + ':00pm ',6 + ':00pm ',7 + ':00am ',8 + ':00pm '];
+// var am = [':00am'];
+// var pm = [':00pm'];
   for (var i = 0; i < 15; i++){
   var new_li = document.createElement('li');
   var cookiesThisHr = firstAndPike.estCookieSalesPerHrArray[i];
-  new_li.textContent = `${i + 6}:00am - ${cookiesThisHr} cookies`;
+  new_li.textContent = `${0 + times[i]} - ${cookiesThisHr} cookies`;
   firstAndPike_ul.appendChild(new_li);
   console.log(cookiesThisHr);
   
