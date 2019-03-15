0acb70a802a07d8250d892a53f01c75c5e1e7167
Williams Argenal <williamsargenal@gmail.com>
2019-03-15 16:14:36 -0700

    trying to figure this out lol

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // global varabiles //
 var times = [6 + ':00am ',7 + ':00am ',8 + ':00am ',9 + ':00am ',10 + ':00am ',11 + ':00am ',12 + ':00pm ',1 + ':00pm ',2 + ':00pm ',3 + ':00pm ',4 + ':00pm ',5 + ':00pm ',6 + ':00pm ',7 + ':00am ',8 + ':00pm '];
 
+var loctions= [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
 
 // math method to round our number//
 function getRandomIntInclusive(min, max) {
@@ -31,7 +32,7 @@ var tr_element1 = document.createElement('tr');
 
 console.log('logged in randomintinclusive');
 // stores information in an object form//
-var firstAndPike = {
+locations.firstAndPike = {
    minCustPerHr: 23,
     maxCustPerHr: 65,
     avgCookieSale: 6.3,
@@ -120,9 +121,7 @@ seaTacAirport.renderAsUl();
 
 
 
-
 //===============================================================================//
-
 
 
 var seattleCenter = {
@@ -241,6 +240,64 @@ for (var i = 0; i < 15; i++){
  console.log(alki);
 };
 alki.renderAsUl();
+
+
+
+
+//==========================================//
+
+
+var total = {
+  minCustPerHr: ,
+   maxCustPerHr: 65,
+   avgCookieSale: 6.3,
+   estCookieSalesPerHrArray: [],
+};
+alki.name = 'total';
+
+
+alki.calculateSalesPerHour = function(){
+   for(var i = 0; i < 15; i++){
+     var randomCustomersPerHour = getRandomIntInclusive (this.minCustPerHr, this.maxCustPerHr);
+     this.estCookieSalesPerHrArray.push(randomCustomersPerHour);
+     console.log('testing');
+   }
+ };
+ alki.calculateSalesPerHour();
+
+alki.renderAsUl = function(){
+var alki_ul = document.getElementById('alki');
+var title_li = document.createElement('th');
+title_li.textContent = 'Alki';
+alki_ul.appendChild(title_li);
+
+for (var i = 0; i < 15; i++){
+ var new_li = document.createElement('th');
+ var cookiesThisHr = this.estCookieSalesPerHrArray[i];
+ new_li.textContent = `${cookiesThisHr}`;
+ alki_ul.appendChild(new_li);
+ console.log(cookiesThisHr);
+ };
+ console.log(alki);
+};
+alki.renderAsUl();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //=================================================//
 
---
+++ new.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// make objects for each location //
+
+
+// key, vaules and properties //
+// 1. location 
+//2. min customer per hr
+// 3. max customers per hr
+// 4. avg cookies per customer
+
+
+// and so key vaule is gonna have a method //
+// 1. Calculate random number of customers
+//2. Calculate average sales per hour
+//3. Calculate total sales per day
+//4. List details in an ordered manner
+//5. Render results onto webpage
+
+
+// math that makes our table work//
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
+
+//Global variable
+var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+//using a business array but using the new method to add new store to table//
+
+var businessesArray = [
+  new Business('1st and Pike', 23, 65, 6.3),
+  new Business('SeaTac Airport', 3, 24, 1.2),
+  new Business('Seattle Center', 11, 38, 3.7),
+  new Business('Capitol Hill', 20, 38, 2.3),
+  new Business('Alki', 23, 65, 6.3)
+]; 
+
+// create my constructor function aka make a object into a function //
+function Business (location, min, max, avgCookie) {
+  this.location = location;
+  this.minCustPerHr = min;
+  this.maxCustPerHr = max;
+  this. avgCookieSale = avgCookie;
+  this.numberCustomersArray = [];
+  this.avgSalesPerHr = [];
+}
+
+//Create methods for constructors
+//This method will calculate random number of customers per hour
+Business.prototype.numberCustomersArray = function() {
+  var randomCustomers = getRandomIntInclusive(this.minCustPerHr, this.maxCustPerHr);
+  return randomCustomers;
+};
